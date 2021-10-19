fbadf152c1fb8cdc7fa315550b3c93f534f8a5ae
qinghe <921525492@qq.com>
2021-10-19 10:22:36 +0800

    ok

--- GAN_demo.py
+++ GAN_demo.py
@@ -3,7 +3,6 @@ import tensorflow as tf
 from scipy.stats import norm
 from matplotlib import pyplot as plt
 
-
 # 真实数据样本的分布
 class DataDistribution(object):
     def __init__(self):
@@ -29,9 +28,9 @@ class GeneratorDistribution(object):
 def linear(input, output_dim, scope=None, stddev = 1.0):
     norm = tf.random_normal_initializer(stddev=stddev)
     const = tf.constant_initializer(0.0)
-    with tf.variable_creator_scope(scope or "linear"):
-        w = tf.compat.v1.get_variable('w',[input.get_shape()[1], output_dim], initializer=norm)
-        b = tf.compat.v1.get_variable("b", [output_dim], initializer= const)
+    with tf.variable_scope(scope or "linear"):
+        w = tf.get_variable('w',[input.get_shape()[1], output_dim], initializer=norm)
+        b = tf.get_variable("b", [output_dim], initializer= const)
         return tf.matmul(input, w)+b
 
 def generator(input, h_dim):
@@ -51,7 +50,7 @@ def optimizer(loss, var_list, initial_learning_rate):
     decay = 0.9
     num_decay_steps = 150
     batch= tf.Variable(0)
-    learning_rate = tf.compat.v1.train.exponential_decay(
+    learning_rate = tf.train.exponential_decay(
         initial_learning_rate,
         batch,
         num_decay_steps,
@@ -59,7 +58,7 @@ def optimizer(loss, var_list, initial_learning_rate):
         staircase=True
     )
 
-    optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate).minimize(
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(
         loss,
         global_step=batch,
         var_list = var_list
@@ -82,19 +81,19 @@ class GAN(object):
 # 创建模型，预判别器D_pre、生成器Generator和判别器，按照之前的公式定义生成器和判别器的损失函数los s g 与loss_d 以及它们的优化器opt_g 与opt_d ，其中Dl 与D2 分别代表真实数据与生成数据的判别。
     def _create_model(self):
 
-        with tf.compat.v1.variable_scope('D_pre'):
-            self.pre_input = tf.compat.v1.placeholder(tf.float32, shape=(self.batch_size, 1))
-            self.pre_labels = tf.compat.v1.placeholder(tf.float32, shape =(self.batch_size, 1))
+        with tf.variable_scope('D_pre'):
+            self.pre_input = tf.placeholder(tf.float32, shape=(self.batch_size, 1))
+            self.pre_labels = tf.placeholder(tf.float32, shape =(self.batch_size, 1))
             D_pre = discriminator(self.pre_input, self.mlp_hidden_size)
             self.pre_loss = tf.reduce_mean(tf.square(D_pre -self.pre_labels))
             self.pre_opt = optimizer(self.pre_loss, None, self.learning_rate)
 
-        with tf.compat.v1.variable_scope("Generator"):
-            self.z = tf.compat.v1.placeholder(tf.float32, shape=(self.batch_size, 1))
+        with tf.variable_scope("Generator"):
+            self.z = tf.placeholder(tf.float32, shape=(self.batch_size, 1))
             self.G = generator(self.z, self.mlp_hidden_size)
         
-        with tf.compat.v1.variable_scope("Discriminator") as scope:
-            self.x = tf.compat.v1.placeholder(tf.float32, shape=(self.batch_size, 1))
+        with tf.variable_scope("Discriminator") as scope:
+            self.x = tf.placeholder(tf.float32, shape=(self.batch_size, 1))
             self.D1 = discriminator(self.x, self.mlp_hidden_size)
             scope.reuse_variables()
             self.D2 = discriminator(self.G, self.mlp_hidden_size)
@@ -102,37 +101,37 @@ class GAN(object):
         self.loss_d =  tf.reduce_mean(-tf.log(self.D1) - tf.log(1 -self.D2))
         self.loss_g = tf.reduce_mean(-tf.log(self.D2))
 
-        self.d_pre_params = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.TRAINABLE_VARIABLES,scope="D_pre")
-        self.d_params = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.TRAINABLE_VARIABLES, scope="Discriminator")
-        self.g_params = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.TRAINABLE_VARIABLES, scope="Generator")
+        self.d_pre_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope="D_pre")
+        self.d_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="Discriminator")
+        self.g_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="Generator")
         
         self.opt_d = optimizer(self.loss_d, self.d_params, self.learning_rate)
         self.opt_g = optimizer(self.loss_g, self.g_params, self.learning_rate)
 
-
+#训练模型
     def train(self):
-        with tf.compat.v1.Session() as session:
-            tf .compat.v1.global_variables_initializer().run()
+        with tf.Session() as session:
+            tf.global_variables_initializer().run()
 
             #pretraining discriminator
             num_pretrain_steps = 1000
             for step in range(num_pretrain_steps):
                 d = (np.random.random(self.batch_size) - 0.5) * 10.0
                 labels = norm.pdf(d, loc=self.data.mu, scale =self.data.sigma)
-                pretrain_loss,_ =  session.run([self.preloss, self.pre_opt], {
+                pretrain_loss,_ =  session.run([self.pre_loss, self.pre_opt], {
                     self.pre_input: np.reshape(d, (self.batch_size, 1)),
                     self.pre_labels: np.reshape(labels, (self.batch_size, 1))
                 })
             self.weightsD = session.run(self.d_pre_params)
             for i,v in enumerate(self.d_params):
-                session.run(v.assigin(self.weightsD[i]))
+                session.run(v.assign(self.weightsD[i]))
 
             for step in range(self.num_steps):
                 #update discriminator
-                X = self.data.sample(self.batch_size)
+                x = self.data.sample(self.batch_size)
                 z = self.gen.sample(self.batch_size)
-                loss_d,_ = session.run([self.loss_d, self, self.opt_d], {
-                    self.X:np.reshape(X, (self.batch_size, 1)),
+                loss_d,_ = session.run([self.loss_d, self.opt_d], {
+                    self.x:np.reshape(x, (self.batch_size, 1)),
                     self.z:np.reshape(z, (self.batch_size, 1))
                 })
 
@@ -146,7 +145,8 @@ class GAN(object):
                     print('{}:{}\t{}'.format(step, loss_d, loss_g))
                 if step % 100 == 0 or step == 0 or step == self.num_steps -1:
                     self._plot_distribution(session)
-                
+
+# 可视化                
     def _samples(self, session, num_points = 10000, num_bins = 100):
         xs = np.linspace(-self.gen.range, self.gen.range, num_points)
         bins = np.linspace(-self.gen.range, self.gen.range ,num_bins)
@@ -180,8 +180,8 @@ class GAN(object):
         plt.ylabel('Probability Density')
         plt.legend()
         plt.show()
-
-def main (arg):
+# 迭代次数为1200 可视化间隔10
+def main ():
     model = GAN(
         DataDistribution(),
         GeneratorDistribution(range=8),
